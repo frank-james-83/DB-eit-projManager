d2f1b0ebbdfdf7f28d0d4533d1a8f591af3c9765
frank-james-83 <frankturkey@163.com>
2025-08-14 17:15:51 +0800

    部署时报错处理

--- env.d.ts
+++ env.d.ts
@@ -4,3 +4,13 @@ declare module '*.vue' {
 	export default component
 }
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  // 在这里添加你的环境变量类型定义
+  readonly VITE_API_URL: string;
+  // 其他环境变量...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
