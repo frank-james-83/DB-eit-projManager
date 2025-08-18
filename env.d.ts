93e6ac0ef793410a4f02583bcb51359438850c1a
frank-james-83 <frankturkey@163.com>
2025-08-18 09:56:54 +0800

    revert

--- env.d.ts
+++ env.d.ts
@@ -4,13 +4,3 @@ declare module '*.vue' {
 	export default component
 }
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  // 在这里添加你的环境变量类型定义
-  readonly VITE_API_URL: string;
-  // 其他环境变量...
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
